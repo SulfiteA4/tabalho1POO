9006922c34f672248fee8d9f9ba484d5599a2132
Rafael Yudi <rafael.tanaka@unesp.br>
2022-11-09 17:41:52 -0300

    Classe pagamento

--- trabalho1POO/src/modelos/Pagamento.java
+++ trabalho1POO/src/modelos/Pagamento.java
@@ -8,6 +8,22 @@ package modelos;
  *
  * @author Martines
  */
-public class Pagamento {
+public abstract class Pagamento {
+    protected String tipoPagamento; 
+
+    public Pagamento(String tipoPagamento) {
+        this.tipoPagamento = tipoPagamento;
+    }
+
+    public String getTipoPagamento() {
+        return tipoPagamento;
+    }
+
+    public void setTipoPagamento(String tipoPagamento) {
+        this.tipoPagamento = tipoPagamento;
+    }
+
+    @Override
+    public abstract String toString();
     
 }
